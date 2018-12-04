fee3f09c5bad5a595c07f5101ecf08377b86485d
Your Name <natalia.sadowska@onet.eu>
2018-12-04 03:04:10 +0100

    element factory created

--- src/main/java/factories/ElementFactoryImpl.java
+++ src/main/java/factories/ElementFactoryImpl.java
@@ -1,13 +1,43 @@
 package factories;
 
 import models.Element;
-import models.Home;
+import models.Position;
+import models.Size;
+import models.properties.ElementProperty;
 
-import java.util.List;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class ElementFactoryImpl implements ElementFactory {
     @Override
     public Element createElement(String line, String separator) {
         // create one element by line
+        String[] values = line.split(separator);
+        Map<ElementProperty, String> elementDetails = new EnumMap<>(ElementProperty.class);
+        elementDetails.put(ElementProperty.ELEMENT_NAME, values[0]);
+        elementDetails.put(ElementProperty.POSITION_X, values[1]);
+        elementDetails.put(ElementProperty.POSITION_Y, values[2]);
+        elementDetails.put(ElementProperty.POSITION_Z, values[3]);
+        elementDetails.put(ElementProperty.LENGTH, values[4]);
+        elementDetails.put(ElementProperty.HEIGHT, values[5]);
+        elementDetails.put(ElementProperty.WIDTH, values[6]);
+        elementDetails.put(ElementProperty.WEIGHT, values[7]);
+
+        String str = elementDetails.get(ElementProperty.ELEMENT_NAME);
+        Position pos = new Position(
+                Float.parseFloat(elementDetails.get(ElementProperty.POSITION_X)),
+                Float.parseFloat(elementDetails.get(ElementProperty.POSITION_Y)),
+                Float.parseFloat(elementDetails.get(ElementProperty.POSITION_Z))
+        );
+        Float wei = Float.parseFloat(elementDetails.get(ElementProperty.WEIGHT));
+        Size siz = new Size(
+                Float.parseFloat(elementDetails.get(ElementProperty.LENGTH)),
+                Float.parseFloat(elementDetails.get(ElementProperty.WIDTH)),
+                Float.parseFloat(elementDetails.get(ElementProperty.HEIGHT))
+        );
+
+        return new Element(str, pos, wei, siz);
     }
+
+
 }
--- src/main/java/factories/RoomFactoryImpl.java
+++ src/main/java/factories/RoomFactoryImpl.java
@@ -1,8 +1,12 @@
 package factories;
 
 import models.Room;
+import models.properties.ElementProperty;
+import models.properties.RoomProperty;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class RoomFactoryImpl implements RoomFactory {
     ElementFactory elementFactory = new ElementFactoryImpl();
@@ -10,5 +14,11 @@ public class RoomFactoryImpl implements RoomFactory {
     @Override
     public Room createRoom(List<String> lines, String separator) {
         // create room using element Factory to create each element
+
+        List<String> values = lines.split(separator);
+        Map<RoomProperty, String> roomDetails = new EnumMap<>(RoomProperty.class);
+        roomDetails.put(ElementProperty.ROOM_NAME, values[0]);
+
+        return new Room(name, colorHex, area, height, elements);
     }
 }
