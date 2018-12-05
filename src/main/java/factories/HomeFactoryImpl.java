584d8b1e04517f001967eef6404a682507c15898
Your Name <natalia.sadowska@onet.eu>
2018-12-05 01:06:09 +0100

    'weird

--- src/main/java/factories/HomeFactoryImpl.java
+++ src/main/java/factories/HomeFactoryImpl.java
@@ -1,8 +1,15 @@
 package factories;
 
+import models.Element;
 import models.Home;
+import models.Room;
+import models.properties.HomeProperty;
+import models.properties.RoomProperty;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class HomeFactoryImpl implements HomeFactory {
     RoomFactory roomFactory = new RoomFactoryImpl();
@@ -10,5 +17,20 @@ public class HomeFactoryImpl implements HomeFactory {
     @Override
     public Home createHome(List<String> lines, String separator) {
         //create home using roomFactory
+        String[] roomLine = lines.get(0).split(separator);
+
+        Map<HomeProperty, String> homeDetails = new EnumMap<>(HomeProperty.class);
+        homeDetails.put(HomeProperty.HOME_NAME, roomLine[0]);
+        homeDetails.put(HomeProperty.ADDRESS, roomLine[1]);
+        homeDetails.put(HomeProperty.HOMEMADE_COUNT, roomLine[2]);
+        homeDetails.put(HomeProperty.ROOMS_COUNT, roomLine[3]);
+
+        int countRooms = Integer.parseInt(homeDetails.get(HomeProperty.ROOMS_COUNT));
+        List<Element> roomsList = new ArrayList<>();
+        for (int i = 1; i < countRooms; i++) {
+            Room room = RoomFactory.createRoom(lines.get(i), separator);
+            roomsList.add(room);
+        }
+        return new Home(name, address, householdCount, rooms);
     }
 }
