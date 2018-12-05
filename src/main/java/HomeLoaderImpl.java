5d927a9dea959e8644b234be120ececb0a62c642
Your Name <natalia.sadowska@onet.eu>
2018-12-06 00:26:04 +0100

    home loader halfway

--- src/main/java/HomeLoaderImpl.java
+++ src/main/java/HomeLoaderImpl.java
@@ -1,6 +1,15 @@
 import factories.HomeFactory;
 import factories.HomeFactoryImpl;
+
 import models.Home;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HomeLoaderImpl implements HomeLoader {
     HomeFactory homeFactory = new HomeFactoryImpl();
@@ -8,6 +17,39 @@ public class HomeLoaderImpl implements HomeLoader {
     @Override
     public Home loadHome(String fileName, String separator) {
         //load file and use home factory to create home
-        return null;
+
+        FileReader fr = null;
+        String line = "";
+
+        // OTWIERANIE PLIKU
+        try {
+            fr = new FileReader("home");
+        } catch (FileNotFoundException e) {
+            System.exit(1);
+        }
+
+        BufferedReader bfr = new BufferedReader(fr);
+        List<String> strList = new ArrayList<>();
+        // ODCZYT KOLEJNYCH LINII Z PLIKU I ZAPIS DO LISTY
+        try {
+            while ((line = bfr.readLine()) != null) {
+                strList.add(line);
+            }
+        } catch (IOException e) {
+            System.exit(2);
+        }
+
+        // ZAMYKANIE PLIKU
+        try {
+            fr.close();
+        } catch (IOException e) {
+            System.exit(3);
+        }
+
+
+        Home home = homeFactory.createHome(strList, separator);
+
+
+        return home;
     }
 }
